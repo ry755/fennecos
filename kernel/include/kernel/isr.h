dead3435f5ec2bfc2c36ad8a47a9d7757ab0b40e
Ry <ryanst755@gmail.com>
2023-04-04 23:08:03 -0700

    kernel/isr: Installable interrupt handlers

--- kernel/include/kernel/isr.h
+++ kernel/include/kernel/isr.h
@@ -2,4 +2,9 @@
 
 #include <stdint.h>
 
+typedef void (*irq_handler_t)();
+
+void install_interrupt_handler(uint8_t irq, irq_handler_t handler);
+void uninstall_interrupt_handler(uint8_t irq);
 void interrupt_handler(uint8_t irq, uint32_t error);
+void exception_handler(uint8_t irq, uint32_t error);
--- kernel/isr.c
+++ kernel/isr.c
@@ -4,6 +4,30 @@
 #include <stdint.h>
 #include <stdio.h>
 
+irq_handler_t irq_handlers[16] = { 0 };
+
+void install_interrupt_handler(uint8_t irq, irq_handler_t handler) {
+    irq_handlers[irq] = handler;
+}
+
+void uninstall_interrupt_handler(uint8_t irq) {
+    irq_handlers[irq] = 0;
+}
+
 void interrupt_handler(uint8_t irq, uint32_t error) {
+    if (irq < 32) {
+        exception_handler(irq, error);
+    } else {
+        void (*handler)(uint8_t irq, uint32_t error) = irq_handlers[irq - 32];
+        if (handler) {
+            handler(irq, error);
+        } else {
+            kprintf("unhandled interrupt! irq: %d, error: %x\n", irq, error);
+        }
+    }
     end_of_interrupt(irq);
+}
+
+void exception_handler(uint8_t irq, uint32_t error) {
+    kprintf("fatal exception! irq: %d, error: %x\n", irq, error);
 }
