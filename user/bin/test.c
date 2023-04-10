72a7d05dbe6fe4c2be5790087553f51ace7137f4
Ry <ryanst755@gmail.com>
2023-04-09 18:37:54 -0700

    user/test: Press ESC to exit the process

--- user/bin/test.c
+++ user/bin/test.c
@@ -2,11 +2,14 @@
 #include <user/framebuffer.h>
 #include <user/keyboard.h>
 
+#include <stdbool.h>
+
 event_t event;
+bool running = true;
 
 void _start() {
-    draw_string("test", 16, 16, 0xFFFFFFFF, 0xFF123456, global_font);
-    while (1) {
+    draw_string("keyboard test - press ESC to exit", 16, 16, 0xFFFFFFFF, 0xFF123456, global_font);
+    while (running) {
         if (get_next_event(&event)) {
             switch (event.type) {
                 case KEY_DOWN:
@@ -19,6 +22,8 @@ void _start() {
                     draw_string("key up  ", 16, 32, 0xFFFFFFFF, 0xFF123456, global_font);
                     keyboard_event(event.arg0);
                     draw_font_tile(scancode_to_ascii(event.arg0 & 0x7F), 16, 48, 0xFFFFFFFF, 0xFF123456, global_font);
+                    if (event.arg0 == 0x81)
+                        running = false;
                     break;
             }
         }
