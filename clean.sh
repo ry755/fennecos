bf444e54e89402f7175dbe92af2012ab24650384
Ry <ryanst755@gmail.com>
2025-12-15 18:32:23 -0800

    Organize source tree and move to a Makefile-based build process

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf build/
-rm -rf base_image/app/*.app
-rm -rf base_image/sys/*.app
-rm -f base_image/boot/kernel.elf
-rm -f base_image/boot/kernel.sym
-rm -f boot.img
-rm -f floppy.img
